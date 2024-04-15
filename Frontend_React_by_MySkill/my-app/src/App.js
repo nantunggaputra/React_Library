62c6d570ee125c050637e028c240c700a5011a18
nantunggaputra <nantunggaputra@github.com>
2024-04-15 21:37:35 +0700

    CRA

--- Frontend_React_by_MySkill/my-app/src/App.js
+++ Frontend_React_by_MySkill/my-app/src/App.js
@@ -5,6 +5,33 @@ import Body from "./Body";
 import ClassComponents from "./ClassComponents";
 import FunctiopnalComponents from "./FunctionalComponents";
 import Chatbox from "./Chatbox";
+import Product from "./Product";
+
+const eBookData = [
+  {
+    name: "eBook HTML from Zero to Hero",
+    url: "https://simpleicons.org/icons/html5.svg",
+    price: 75000,
+    discount: 4,
+  },
+  {
+    name: "eBook CSS from Zero to Hero",
+    url: "https://simpleicons.org/icons/html5.svg",
+    price: 90000,
+    discount: 20,
+  },
+  {
+    name: "eBook JavaScript from Zero to Hero",
+    url: "https://simpleicons.org/icons/javascript.svg",
+    price: 100000,
+    discount: 12,
+  },
+  {
+    name: "eBook React from Zero to Hero",
+    url: "https://simpleicons.org/icons/react.svg",
+    price: 100000,
+  },
+];
 
 function App() {
   return (
@@ -25,6 +52,38 @@ function App() {
           Learn React
         </a>
         <Chatbox />
+        <h2>PDF Front-End Development</h2>
+        {eBookData.map((pdf, id) => (
+          <Product
+            name={pdf.name}
+            url={pdf.url}
+            price={pdf.price}
+            discount={pdf.discount}
+          />
+        ))}
+        {/* <Product
+          name="eBook HTML from Zero to Hero"
+          url="https://simpleicons.org/icons/html5.svg"
+          price="75000"
+          discount="4"
+        />
+        <Product
+          name="eBook CSS from Zero to Hero"
+          url="https://simpleicons.org/icons/css3.svg"
+          price="90000"
+          discount="20"
+        />
+        <Product
+          name="eBook JavaScript from Zero to Hero"
+          url="https://simpleicons.org/icons/javascript.svg"
+          price="100000"
+          discount="12"
+        />
+        <Product
+          name="eBook React from Zero to Hero"
+          url="https://simpleicons.org/icons/react.svg"
+          price="100000"
+        /> */}
       </header>
     </div>
   );
---
+++ Frontend_React_by_MySkill/my-app/src/Product.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Product({ price, name, url, discount }) {
+  return (
+    <div className="product">
+      <h5>{name}</h5>
+      <img src={url} alt={Math.random()} />
+      <br />
+      <small>
+        {!discount ? (
+          <b>Rp. {price}</b>
+        ) : (
+          <>
+            <s>Rp. {price}</s> <span className="discount">(-{discount}%)</span>
+          </>
+        )}
+      </small>
+      {!discount ? (
+        <></>
+      ) : (
+        <small>
+          <br />
+          <b>
+            Rp.{" "}
+            {parseFloat(price) -
+              parseFloat((parseInt(price) * parseInt(discount)) / 100)}
+          </b>
+        </small>
+      )}
+    </div>
+  );
+}
+
+export default Product;
