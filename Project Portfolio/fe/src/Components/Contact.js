559c36d16f5e8aef70bd7833d7e2b582e2e7c95a
nantunggaputra <nantunggaputra@github.com>
2024-05-10 23:56:34 +0700

    FE React Portfolio

---
+++ Project Portfolio/fe/src/Components/Contact.js
@@ -0,0 +1,121 @@
+import { Component } from "react";
+import { Fade, Slide } from "react-awesome-reveal";
+
+class Contact extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const street = this.props.data.address.street;
+    const city = this.props.data.address.city;
+    const state = this.props.data.address.state;
+    const zip = this.props.data.address.zip;
+    const message = this.props.data.contactmessage;
+
+    return (
+      <section id="contact">
+        <Fade bottom duration={1000}>
+          <div className="row section-head">
+            <div className="two columns header-col">
+              <h1>
+                <span>Let's Connect</span>
+              </h1>
+            </div>
+            <div className="ten columns">
+              <p className="lead">{message}</p>
+            </div>
+          </div>
+        </Fade>
+        <div className="row">
+          <Slide left duration={1300}>
+            <div className=" eight columns">
+              <form action="" method="post" id="contactForm">
+                <fieldset>
+                  <div>
+                    <label htmlFor="contactName">
+                      Name <span className="require">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactName"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="contactEmail">
+                      Email <span className="require">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactEmail"
+                      name="contactEmail"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="contactSubject">
+                      Subject <span className="require">*</span>{" "}
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue=" "
+                      size="35"
+                      id="contactSubject"
+                      name="contactSubject"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="contactMessage">
+                      Message <span className="require">*</span>{" "}
+                    </label>
+                    <textarea
+                      cols="50"
+                      rows="10"
+                      id="contactMessage"
+                      name="contactMessage"
+                      resize="false"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="submit">Submit</button>
+                    <span id="image-loader">
+                      <img src="images/loader.gif" alt="Submit" />
+                    </span>
+                  </div>
+                </fieldset>
+              </form>
+
+              <div id="message-warning">Error</div>
+              <div id="message-success">
+                <i className="fa fa-check"></i>Your Message
+                <br />
+              </div>
+            </div>
+          </Slide>
+
+          <Slide right duration={1000}>
+            <aside className="four columns footer-widgets">
+              <div className="widget widget_contact">
+                <h4>Address and Phone</h4>
+                <p className="address">
+                  {name}
+                  <br />
+                  {street}
+                  <br />
+                  {city}, {state}, {zip}
+                </p>
+              </div>
+            </aside>
+          </Slide>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact;
